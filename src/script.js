e565bf4dde552dc37117ab89b034d9e9f0b5fcf4
gestagene <gestagenebrian@gmail.com>
2025-12-17 18:08:55 +0800

    v1

---
+++ src/script.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  }
+);
+
+sections.forEach((section) => observer.observe(section));
